5cc9357748e2a97207846a88d0bdd4d65bd61699
sumbobyboys <sumbobyboys@f9af527e-e11e-11de-9fd5-4be1c9d5bc18>
2009-12-06 21:51:37 +0000

    test dans le main

--- trunk/Projet POLY/main.cpp
+++ trunk/Projet POLY/main.cpp
@@ -4,12 +4,26 @@
 #include "Assembler.hpp"
 #include "Interpreter.h"
 
+
+#define TAILLE_MEMOIRE 512
+
+
 using namespace std;
 
 int main (int argc, char * const argv[]) {
+	/*
 	int a, b, c ,d, e;
 	parseFile("test.txt");
 	parseHexa(0x0207b800, &a, &b, &c, &d, &e);
 	loadMemory("test2.txt");
+	*/
+	
+	
+	parseFile("test.txt");
+	unsigned int* prog = new unsigned int[TAILLE_MEMOIRE];
+	prog = loadMemory("test3.txt");
+	execMemory(prog);
+	
+	
     return 0;
 }
